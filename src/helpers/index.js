76029f563a63df8b1cf51b79532429f73227f81e
Teodora <tedy.aa@abv.bg>
2025-08-16 12:54:16 +0300

    added more helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -14,3 +14,16 @@ export function dateBuilderDDMY(d) {
   let year = d.getFullYear()
   return `${day} ${date} ${month} ${year}`;
 }
+
+export function dateBuilderDD(timestamp) {
+  const date = new Date(timestamp * 1000);
+
+  const weekday = date.toLocaleDateString('en-US', { weekday: 'short' });
+  const day = date.getDate();
+
+  return `${weekday} ${day}`;
+}
+
+export function kelvinToCelsius(temp){
+  return Math.round((temp - 272))
+}
