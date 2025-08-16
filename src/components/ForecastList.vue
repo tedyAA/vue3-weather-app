68f67d4d7a93d7f9a5444f4543f9acd6e8bcbca2
Teodora <tedy.aa@abv.bg>
2025-08-16 14:12:36 +0300

    refactor

--- src/components/ForecastList.vue
+++ src/components/ForecastList.vue
@@ -1,7 +1,7 @@
 <template>
-  <ForecastCardLoading v-if="!hasForecast && store.loading"/>
-  <div class="flex justify-center mt-10" v-else>
-    <div class="grid w-fit gap-4 grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
+  <div class="flex justify-center mt-10">
+    <ForecastCardLoading v-if="store.loading"/>
+    <div v-else class="grid w-fit gap-4 grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
       <div v-for="(item, index) in store.forecastData" :key="index" class="flex items-center justify-center">
         <ForecastCard :forecast="item" />
       </div>
@@ -12,11 +12,7 @@
 <script setup>
 import { useForecastStore } from '@/stores/forecast.js'
 import ForecastCard from '@/components/ForecastCard.vue'
-import { isEmpty } from 'lodash'
-import { computed } from 'vue'
 import ForecastCardLoading from '@/components/loading/ForecastCardLoading.vue'
 
 const store = useForecastStore()
-
-const hasForecast = computed(() => !isEmpty(store.forecastData))
 </script>
