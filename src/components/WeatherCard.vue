0e31df36f4b29f8c538f2ba0df85d4de281734a8
Teodora <tedy.aa@abv.bg>
2025-08-16 12:31:45 +0300

    refactor

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -6,7 +6,7 @@
       <div class="text-white text-[20px] font-light italic text-center">{{ dateBuilderDDMY() }}</div>
     </div>
     <div class="flex justify-center">
-      <div class="flex justify-center w-fit px-[35px] py-[10px] text-white text-md-[102px] text-[70px] font-black shadow-[3px_6px_rgba(0,0,0,0.25)] bg-[rgba(255,255,255,0.25)] rounded-[16px] my-[30px]">
+      <div class="flex justify-center w-fit px-[35px] py-[10px] text-white text-[70px] md:text-[102px] font-black shadow-[3px_6px_rgba(0,0,0,0.25)] bg-[rgba(255,255,255,0.25)] rounded-[16px] my-[30px]">
         <img
           :src="`https://openweathermap.org/img/wn/${store.weatherData.weather[0].icon}@2x.png`"
           :alt="store.weatherData.weather[0].description"
