065c37c5e69ab1aed0bafac05d301bcde8f901ec
Teodora <tedy.aa@abv.bg>
2025-08-16 12:30:29 +0300

    changed box style

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -5,17 +5,17 @@
       <div class="text-white text-6xl font-medium text-center mt-5">{{ cityName }}</div>
       <div class="text-white text-[20px] font-light italic text-center">{{ dateBuilderDDMY() }}</div>
     </div>
-    <div class="text-center">
-      <div class="inline-block px-[25px] py-[10px] text-white text-[102px] font-black shadow-[3px_6px_rgba(0,0,0,0.25)] bg-[rgba(255,255,255,0.25)] rounded-[16px] my-[30px]">
-        <p>{{ weatherTemp }}°C</p>
+    <div class="flex justify-center">
+      <div class="flex justify-center w-fit px-[35px] py-[10px] text-white text-md-[102px] text-[70px] font-black shadow-[3px_6px_rgba(0,0,0,0.25)] bg-[rgba(255,255,255,0.25)] rounded-[16px] my-[30px]">
         <img
           :src="`https://openweathermap.org/img/wn/${store.weatherData.weather[0].icon}@2x.png`"
           :alt="store.weatherData.weather[0].description"
-          class="mx-auto"
+          class="w-full my-auto h-full"
         />
+        <p>{{ weatherTemp }}°C</p>
       </div>
-      <div class="text-white text-[48px] font-bold italic">{{ weatherProg }}</div>
     </div>
+    <div class="text-white text-center text-[48px] font-bold italic">{{ weatherProg }}</div>
   </div>
 </template>
 
