2c54bcbed5407344e4e7d9131d8789fa764fe836
Teodora <tedy.aa@abv.bg>
2025-08-16 11:58:05 +0300

    added weather card loading sceleton

---
+++ src/components/loading/WeatherCardLoading.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col items-center space-y-6 p-6">
+    <div class="w-full max-w-md space-y-2">
+      <div class="h-4 w-2/3 bg-gray-300 rounded animate-pulse mx-auto"></div>
+      <div class="h-4 w-1/2 bg-gray-300 rounded animate-pulse mx-auto"></div>
+    </div>
+    <div class="h-40 w-full max-w-md bg-gray-300 rounded-xl flex flex-col justify-center items-center space-y-3">
+      <div class="h-5 w-2/3 bg-gray-400 rounded animate-pulse"></div>
+      <div class="h-4 w-1/2 bg-gray-400 rounded animate-pulse"></div>
+    </div>
+    <div class="w-full max-w-md space-y-2">
+      <div class="h-4 w-5/6 bg-gray-300 rounded animate-pulse mx-auto"></div>
+    </div>
+  </div>
+</template>
