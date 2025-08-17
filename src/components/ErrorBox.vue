c0204451f51b9ba5530aa1223cf6d7fa12414381
Teodora <tedy.aa@abv.bg>
2025-08-17 10:56:06 +0300

    added comments to error box

--- src/components/ErrorBox.vue
+++ src/components/ErrorBox.vue
@@ -1,14 +1,17 @@
 <template>
   <p class="text-center font-bold text-2xl font-merriweather text-white mt-8">Ooops!</p>
-<p class="text-white font-extrabold text-9xl text-center font-merriweather">{{store.errorCode}}</p>
-  <p class="text-center font-bold text-2xl font-merriweather text-white">{{store.errorMessage}}</p>
+  <!--  Display error code from store-->
+  <p class="text-white font-extrabold text-9xl text-center font-merriweather">
+    {{ store.errorCode }}</p>
+  <!--  Display error message from store-->
+  <p class="text-center font-bold text-2xl font-merriweather text-white">
+    {{ store.errorMessage }}</p>
 </template>
 
 <script setup>
+// A simple component that shows an error state when a weather API call fails.
+
 import { useWeatherStore } from '@/stores/weather.js'
+
 const store = useWeatherStore()
 </script>
-
-<style scoped>
-
-</style>
