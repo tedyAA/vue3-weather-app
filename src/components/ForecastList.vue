6fc8490bc3c1e35346106a78ff02584a9db42c9c
Teodora <tedy.aa@abv.bg>
2025-08-17 11:01:36 +0300

    added comments to forecast list

--- src/components/ForecastList.vue
+++ src/components/ForecastList.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="flex justify-center mt-10">
-    <ForecastCardLoading v-if="store.loading"/>
+    <!-- Displays loader while the forecast data is still loading -->
+    <ForecastCardLoading v-if="store.loading" />
+    <!--    Loops through forecast if available and renders ForecastCard-->
     <div v-else class="grid w-fit gap-4 grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
-      <div v-for="(item, index) in store.forecastData" :key="index" class="flex items-center justify-center">
+      <div v-for="(item, index) in store.forecastData" :key="index"
+           class="flex items-center justify-center">
         <ForecastCard :forecast="item" />
       </div>
     </div>
@@ -10,6 +13,8 @@
 </template>
 
 <script setup>
+// Simple list component for forecast display
+
 import { useForecastStore } from '@/stores/forecast.js'
 import ForecastCard from '@/components/ForecastCard.vue'
 import ForecastCardLoading from '@/components/loading/ForecastCardLoading.vue'
