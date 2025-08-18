f6781788cb479ffc22505fa6885554033fd9bcca
Teodora <tedy.aa@abv.bg>
2025-08-18 16:07:32 +0300

    fixed text size on mobile

--- src/components/ForecastCard.vue
+++ src/components/ForecastCard.vue
@@ -2,7 +2,7 @@
   <!--  Displays the date of the forecast-->
   <div v-if="forecast"
        class="text-center inline-block px-[25px] py-[10px] text-white text-[16px] font-black shadow-[3px_6px_rgba(0,0,0,0.25)] bg-[rgba(255,255,255,0.25)] rounded-[16px]  mx-0 [text-shadow:3px_6px_rgba(0,0,0,0.25)]">
-    <p class="text-white text-2xl">{{ dateBuilderDD(forecast.dt) }}</p>
+    <p class="text-white text-lg md:text-2xl">{{ dateBuilderDD(forecast.dt) }}</p>
     <!--  Displays the icon of the forecast-->
     <img
       :src="`https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`"
@@ -10,7 +10,7 @@
       class="mx-auto"
     />
     <!--  Displays the min and max temperature of the forecast-->
-    <p class="text-2xl"><span
+    <p class="text-lg md:text-2xl"><span
       class="text-gray-300">{{ kelvinToCelsius(forecast.main.temp_min) }}°</span> /
       {{ kelvinToCelsius(forecast.main.temp_max) }}°</p>
   </div>
